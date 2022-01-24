b05bc52fd3119bb6aaaf98e6c3f9841d5caad2c9
adsonb87 <adsonb87@gmail.com>
2022-01-23 21:25:29 -0300

    Login

--- src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
+++ src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +23,11 @@ public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		.antMatchers("/","/convidados").permitAll()
+		.antMatchers("/","/convidados").hasRole("ADMIN")
+		.anyRequest().authenticated().and().formLogin().permitAll()
+		.and()
+		.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/login").permitAll()
 		.and()
 		.httpBasic();
 	}
@@ -31,7 +36,10 @@ public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.inMemoryAuthentication()
 			.withUser("adson").password(passwordEncoder()
-			.encode("12345")).roles("ADMIN");
+			.encode("12345")).roles("ADMIN")
+			.and()
+			.withUser("jose").password(passwordEncoder()
+			.encode("12345")).roles("USER");
 	
 	}
 	
